9df4902d83b6d65b0bf5e487085f77b433fe8fb9
Remi <paco.cruz.guzman@gmail.com>
2022-04-23 09:53:38 -0500

    Implementación del microservicio store-product-service

---
+++ src/test/java/com/store/product/StoreProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
